9791d76c2dfabb2a973c653d87a73787e5e23ff2
radu200 <raduprodan200gmail.com>
2017-09-16 16:07:50 +0100

    changed layout

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Profile from './profile';
-import {  FormControl, Button} from 'react-bootstrap';
-
+import Books from './books';
+import {FormControl, Button} from 'react-bootstrap';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       query: '',
-      books:null
+      books: null
     }
   }
-  search(){
-  //  console.log('this.state', this.state)
-    const URL = 'https://www.googleapis.com/books/v1/volumes?q=';
-    let FETCH_URL = `${URL}q=${this.state.query}`;
-    fetch(FETCH_URL, {
-      method: 'GET'
-    })
-    .then(response => response.json())
-    .then(json => {
+  search() {
+    console.log('this.state', this.state)
+    const URL = `https://www.googleapis.com/books/v1/volumes?q=${this.state.query}`;
+    fetch(URL, {method: 'GET'}).then(response => response.json()).then(json => {
       const books = json.items[0];
       this.setState({books});
     })
   }
   render() {
     return (
-      <div className="App container">
-         <div className="App-title"><h1>Search for a Book</h1></div>
-          <FormControl
-           className="input"
-            type="text"
-             placeholder="Search "
-             value={this.state.query}
-             onChange={event => {this.setState({query:event.target.value})}}
-             onKeyPress={event =>{
-              if (event.key === 'Enter'){
-              this.search()
-               }
-             }}
-               />
-           <Button className="button" onClick={()=> this.search()} >SEARCH</Button>
-          <Profile books={this.state.books}/>
+      <div className="App container ">
+        <h3 className="text-center">Search For Any Book</h3>
+        <FormControl className="input" type="text" placeholder="Search " value={this.state.query} onChange={event => {
+          this.setState({query: event.target.value})
+        }} onKeyPress={event => {
+          if (event.key === 'Enter') {
+            this.search()
+          }
+        }}/>
+        <Books books={this.state.books}/>
       </div>
     );
   }
---
+++ src/books.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Books extends Component {
+  render() {
+    //console.log('this.props',this.props);
+    let books = {
+      volumeInfo: {
+        imageLinks: {
+          thumbnail: ''
+        },
+        authors: [],
+        title: '',
+        pageCount: '',
+        description: ''
+      }
+    };
+    books = this.props.books !== null
+      ? this.props.books
+      : books;
+    if (this.props.books !== null) {
+      return <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img alt='' src={books.volumeInfo.imageLinks.thumbnail}/>
+          </div>
+          <div className=" about-book col-md-8">
+            <ul className="list-group">
+              <li className=" list-group-item">
+                <strong>Title :</strong>
+                {books.volumeInfo.title}</li>
+              <li className=" list-group-item ">
+                <strong>Author :
+                </strong>{books.volumeInfo.authors}</li>
+              <li className="list-group-item">
+                <strong>Category:
+                </strong>{books.volumeInfo.categories}
+              </li>
+              <li className="list-group-item">
+                <strong>Publisher:
+                </strong>{books.volumeInfo.publisher}
+              </li>
+              <li className="list-group-item">
+                <strong>langauge:
+                </strong>{books.volumeInfo.language}
+              </li>
+              <li className=" list-group-item pages">
+                <strong>Number of Pages :</strong>
+                {books.volumeInfo.pageCount}</li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <br/>
+          <div className="description col-md-12">
+            <ul className="list-group">
+              <li className="list-group-item">
+                <strong>Description
+                </strong>{books.volumeInfo.description}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    } else {
+      return null
+    }
+
+  }
+}
+
+
+export default Books;
